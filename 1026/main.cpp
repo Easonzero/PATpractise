bae20efc5467b726c32ed6e7edc8e33d5d859e10
eason <451114984@qq.com>
2017-08-10 19:14:55 +0800

    1026 = {0};int result[10] = {0};bool check(){    for(int i=0;i<10;i++){ if(origin[i] != result[i]) return false;    }    return true;}void count(int* array,string n){    for(int i=0;i<n.length();i++) array[n[i]-'0']++;}string muti2(string n){    int tmp = 0;    for(int i=n.length()-1;i>=0;i--){        tmp = (n[i]-'0')*2 + tmp/10; if(tmp>9&&i==0) return "0";        n[i] = tmp%10 + '0';    }    return n;}int main(){    string n;    cin >> n;    count(origin,n);    string n2 = muti2(n);    count(result,n2);    if(check()) cout << "Yes" << endl;    else cout << "No" << endl;    cout << n2;    return 0;}1026/main.cpp

--- 1026/main.cpp
+++ 1026/main.cpp
@@ -2,3 +2,137 @@
 // Created by eason on 8/8/17.
 //
 
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+const int START = 8*60*60,END = 21*60*60;
+
+struct Customer{
+    int arrive;
+    int t;
+    int serve;
+    bool vip;
+};
+
+struct Table{
+    int end = START;
+    int n = 0;
+    bool vip = false;
+};
+
+Customer* queue[10010];
+Table* tables[110];
+
+int timeTo(int h,int m,int s){
+    return h*60*60+m*60+s;
+}
+
+void toTime(int t,int* h,int* m,int* s){
+    *h = t/3600;
+    *m = (t-*h*3600)/60;
+    *s = t%60;
+}
+
+bool cmp(Customer* e1,Customer* e2){
+    return e1->arrive<e2->arrive;
+}
+
+bool cmp2(Customer* e1,Customer* e2){
+    return e1->serve<e2->serve;
+}
+
+void print(Customer* e){
+    int h,m,s;
+    toTime(e->arrive,&h,&m,&s);
+    printf("%02d:%02d:%02d ",h,m,s);
+    toTime(e->serve,&h,&m,&s);
+    printf("%02d:%02d:%02d ",h,m,s);
+    cout << (e->serve-e->arrive+30)/60 << endl;
+}
+
+void insertVip(int start,int end){
+    Customer* vip = queue[end];
+    for(int k=end-1;k>=start;k--){
+        queue[k+1] = queue[k];
+    }
+    queue[start] = vip;
+}
+
+int main(){
+    int n,k,m;
+    cin >> n;
+    for(int i=0;i<n;i++){
+        queue[i] = new Customer;
+        int h,m,s;
+        scanf("%d:%d:%d ",&h,&m,&s);
+        cin >> queue[i]->t >> queue[i]->vip;
+        if(queue[i]->t>120) queue[i]->t=120;
+        queue[i]->arrive = timeTo(h,m,s);
+    }
+    cin >> k >> m;
+    for(int i=1;i<=k;i++)
+        tables[i] = new Table();
+    for(int i=0;i<m;i++){
+        int index;
+        cin >> index;
+        tables[index]->vip = true;
+    }
+    sort(queue,queue+n,cmp);
+    for(int i=0;i<n;i++){
+        Customer* e = queue[i];
+        if(e->arrive>=END) break;
+        bool served = false;
+        int tmp = -1,min = END,minIndex = -1;
+        for(int j=1;j<=k;j++){
+            if(tables[j]->end<min){
+                min = tables[j]->end;
+                minIndex = j;
+            }
+            if(e->vip&&e->arrive>=tables[j]->end){
+                if(tables[j]->vip){
+                    tables[j]->end = e->arrive+e->t*60;
+                    tables[j]->n++;
+                    served = true;
+                    e->serve = e->arrive;
+                    break;
+                }else if(tmp==-1) tmp = j;
+            }else if(e->arrive>=tables[j]->end){
+                tables[j]->end = e->arrive+e->t*60;
+                tables[j]->n++;
+                served = true;
+                e->serve = e->arrive;
+                break;
+            }
+        }
+        if(!served&&minIndex!=-1){
+            if(e->vip&&tmp!=-1){
+                tables[tmp]->end = e->arrive+e->t*60;
+                tables[tmp]->n++;
+                e->serve = e->arrive;
+            }else{
+                if(tables[minIndex]->vip&&!e->vip)
+                    for(int j=i+1;j<n;j++){
+                        if(queue[j]->arrive>tables[minIndex]->end)
+                            break;
+                        if(queue[j]->vip)
+                            insertVip(i,j);
+                    }
+                queue[i]->serve = tables[minIndex]->end;
+                tables[minIndex]->end += queue[i]->t*60;
+                tables[minIndex]->n++;
+            }
+        }
+    }
+    sort(queue,queue+n,cmp2);
+    for(int i=0;i<n;i++){
+        if(queue[i]->serve==0) continue;
+        print(queue[i]);
+    }
+    for(int i=1;i<=k;i++){
+        cout << tables[i]->n;
+        if(i!=k) cout << " ";
+    }
+    return 0;
+}
--- test.cpp
+++ test.cpp
@@ -3,44 +3,135 @@
 //
 
 #include <cstdio>
-#include <algorithm>
 #include <vector>
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+
 using namespace std;
-struct student {
-    long long int no;
-    int score, finrank, loca, locarank;
+struct person {
+    int arrive, start, time,index;
+    bool vip;
+}tempperson;
+struct tablenode {
+    int end = 8 * 3600;
+    int num;
+    bool vip;
 };
-bool cmp1(student a, student b) {
-    if(a.score != b.score)
-        return a.score > b.score;
+bool cmp1(person a, person b) {
+    return a.arrive < b.arrive;
+}
+bool cmp2(person a, person b) {
+    return a.start < b.start;
+}
+vector<person> player;
+vector<tablenode> table;
+void alloctable(int personid, int tableid) {
+    if(player[personid].arrive <= table[tableid].end)
+        player[personid].start = table[tableid].end;
     else
-        return a.no < b.no;
+        player[personid].start = player[personid].arrive;
+    table[tableid].end = player[personid].start + player[personid].time;
+    table[tableid].num++;
+    player[personid].index = tableid;
+}
+int findnextvip(int vipid) {
+    vipid++;
+    while(vipid < player.size() && player[vipid].vip == false) {
+        vipid++;
+    }
+    return vipid;
 }
 int main() {
-    int n, m;
+    int n, k, m, viptable;
     scanf("%d", &n);
-    vector<student> fin;
-    for(int i = 1; i <= n; i++) {
-        scanf("%d", &m);
-        vector<student> v(m);
-        for(int j = 0; j < m; j++) {
-            scanf("%lld %d", &v[j].no, &v[j].score);
-            v[j].loca = i;
+    for(int i = 0; i < n; i++) {
+        int h, m, s, temptime, flag;
+        scanf("%d:%d:%d %d %d", &h, &m, &s, &temptime, &flag);
+        tempperson.arrive = h * 3600 + m * 60 + s;
+        tempperson.start = 21 * 3600;
+        if(tempperson.arrive >= 21 * 3600)
+            continue;
+        tempperson.time = temptime <= 120 ? temptime * 60 : 7200;
+        tempperson.vip = ((flag == 1) ? true : false);
+        player.push_back(tempperson);
+    }
+    scanf("%d%d", &k, &m);
+    table.resize(k + 1);
+    for(int i = 0; i < m; i++) {
+        scanf("%d", &viptable);
+        table[viptable].vip = true;
+    }
+    sort(player.begin(), player.end(), cmp1);
+    int i = 0, vipid = -1;
+    vipid = findnextvip(vipid);
+    while(i < player.size()) {
+        int index = -1, minendtime = 999999999;
+        for(int j = 1; j <= k; j++) {
+            if(i==4) cout << table[j].end << endl;
+            if(table[j].end < minendtime) {
+                minendtime = table[j].end;
+                index = j;
+            }
         }
-        sort(v.begin(), v.end(), cmp1);
-        v[0].locarank = 1;
-        fin.push_back(v[0]);
-        for(int j = 1; j < m; j++) {
-            v[j].locarank = (v[j].score == v[j - 1].score) ? (v[j - 1].locarank) : (j + 1);
-            fin.push_back(v[j]);
+        if(table[index].end >= 21 * 3600)
+            break;
+        if(player[i].vip == true && i < vipid) {
+            i++;
+            continue;
+        }
+        if(table[index].vip == true) {
+            if(player[i].vip == true) {
+                alloctable(i, index);
+                if(vipid == i)
+                    vipid = findnextvip(vipid);
+                i++;
+            } else {
+                if(vipid < player.size() && player[vipid].arrive <= table[index].end) {
+                    alloctable(vipid, index);
+                    vipid = findnextvip(vipid);
+                } else {
+                    alloctable(i, index);
+                    i++;
+                }
+            }
+        } else {
+            if(player[i].vip == false) {
+                alloctable(i, index);
+                i++;
+            } else {
+                int vipindex = -1, minvipendtime = 999999999;
+                for(int j = 1; j <= k; j++) {
+                    if(table[j].vip == true && table[j].end < minvipendtime) {
+                        minvipendtime = table[j].end;
+                        vipindex = j;
+                    }
+                }
+                if(vipindex != -1 && player[i].arrive >= table[vipindex].end) {
+                    alloctable(i, vipindex);
+                    if(vipid == i)
+                        vipid = findnextvip(vipid);
+                    i++;
+                } else {
+                    alloctable(i, index);
+                    if(vipid == i)
+                        vipid = findnextvip(vipid);
+                    i++;
+                }
+            }
         }
     }
-    sort(fin.begin(), fin.end(), cmp1);
-    fin[0].finrank = 1;
-    for(int j = 1; j < fin.size(); j++)
-        fin[j].finrank = (fin[j].score == fin[j - 1].score) ? (fin[j - 1].finrank) : (j + 1);
-    printf("%d\n", fin.size());
-    for(int i = 0; i < fin.size(); i++)
-        printf("%013lld %d %d %d\n", fin[i].no, fin[i].finrank, fin[i].loca, fin[i].locarank);
+    sort(player.begin(), player.end(), cmp2);
+    for(i = 0; i < player.size() && player[i].start < 21 * 3600; i++) {
+        cout << player[i].index << " ";
+        printf("%02d:%02d:%02d ", player[i].arrive / 3600, player[i].arrive % 3600 / 60, player[i].arrive % 60);
+        printf("%02d:%02d:%02d ", player[i].start / 3600, player[i].start % 3600 / 60, player[i].start % 60);
+        printf("%.0f\n", round((player[i].start - player[i].arrive) / 60.0));
+    }
+    for(int i = 1; i <= k; i++) {
+        if(i != 1)
+            printf(" ");
+        printf("%d", table[i].num);
+    }
     return 0;
 }
