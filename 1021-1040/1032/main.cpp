2832c044a51bf5cbca63f6f597cf63cdad3e8713
eason <451114984@qq.com>
2017-08-14 21:05:33 +0800

    1032

--- 1021-1040/1032/main.cpp
+++ 1021-1040/1032/main.cpp
@@ -7,41 +7,25 @@
 using namespace std;
 
 int mnext[100000];
-char mdata[100000];
+bool visit[100000];
 
-int getLen(int head){
-    int t = head,len = 0;
-    while(t!=-1){
-        len++;
-        t = mnext[t];
+void travel(int head){
+    int p = head;
+    while(p!=-1){
+        visit[p]=true;
+        p = mnext[p];
     }
-    return len;
 }
 
-int getAddress(int head,int dt){
-    int t = head;
-    while(dt!=0){
-        t = mnext[t];
-        dt--;
-    }
-    return t;
-}
+int chkSharing(int head){
+    int p=head;
 
-int chkSharing(int head1,int head2){
-    int sharing=-1,p1=head1,p2=head2;
-    bool flag = false;
-    while(p1!=p2){
-        flag = false;
-        p1 = mnext[p1];
-        p2 = mnext[p2];
-        if(p1==p2&&!flag){
-            sharing = p1;
-            flag = true;
-        }
+    while(p!=-1){
+        if(visit[p]) return p;
+        p = mnext[p];
     }
 
-    if(!flag) sharing=-1;
-    return sharing;
+    return -1;
 }
 
 int main(){
@@ -54,21 +38,11 @@ int main(){
         char data;
         cin >> address >> data >> nextAddress;
         mnext[address] = nextAddress;
-        mnext[address] = nextAddress;
-        mdata[address] = data;
     }
 
-    int len1 = getLen(head1),len2 = getLen(head2);
-    int sharing;
-    if(len1>len2){
-        int address = getAddress(head1,len1-len2);
-        sharing = chkSharing(address,head2);
-    }else{
-        int address = getAddress(head2,len2-len1);
-        sharing = chkSharing(head1,address);
-    }
-
-    cout << sharing;
-
+    travel(head1);
+    int sharing = chkSharing(head2);
+    if(sharing!=-1) printf("%05d",sharing);
+    else cout << -1;
     return 0;
 }
--- 1021-1040/1033/main.cpp
+++ 1021-1040/1033/main.cpp
@@ -2,3 +2,90 @@
 // Created by eason on 8/13/17.
 //
 
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+const int MAX = 99999;
+
+struct Station{
+    float distance;
+    float price;
+};
+
+bool cmp(Station* s1,Station* s2){
+    return s1->distance < s2->distance;
+}
+
+vector<Station*> v;
+
+int main(){
+    int c,d,n;
+    float avg;
+    cin >> c >> d >> avg >> n;
+
+    for(int i=0;i<n;i++){
+        auto station = new Station();
+        cin >> station->price >> station->distance;
+        v.push_back(station);
+    }
+
+    sort(v.begin(),v.end(),cmp);
+
+    float reach=0,preprice=0,total=0,min = MAX,ctmp=c,cmin=MAX;
+    for(auto station:v){
+        float dt = station->distance-reach;
+        if(station->price<=preprice||station->distance==0){
+            if(dt > ctmp*avg){
+                total += ctmp*preprice;
+                reach += ctmp*avg;
+                total +=
+                preprice = station->price;
+                min = MAX;
+                ctmp = c;
+            }else{
+                total += dt*preprice/avg;
+                reach = station->distance;
+                preprice = station->price;
+                min = MAX;
+                ctmp = c;
+            }
+        }else if(dt > ctmp*avg&&min!=MAX){
+            total += ctmp*preprice;
+            reach += ctmp*avg;
+            if(min < station->price){
+                preprice = min;
+                ctmp = cmin;
+                min = station->price;
+                cmin = (station->distance-reach)/avg;
+            }else{
+                total += (dt-ctmp*avg)*min/avg;
+                reach = station->distance;
+                preprice = station->price;
+                min = MAX;
+                ctmp = c;
+            }
+        }else if(dt > ctmp*avg&&min==MAX){
+            printf("The maximum travel distance = %.2f",reach+ctmp*avg);
+        }else{
+            if(station->price<min) {
+                min = station->price;
+                cmin = (station->distance-reach)/avg;
+            }
+        }
+    }
+    float dt = d-reach;
+    if(dt>ctmp*avg&&min==MAX){
+        printf("The maximum travel distance = %.2f",reach+ctmp*avg);
+    }else{
+        if(dt<=ctmp*avg){
+            total += dt*preprice/avg;
+        }else{
+            total += ctmp*preprice + (dt-ctmp*avg)*min;
+        }
+        printf("%.2f",total);
+    }
+    return 0;
+}
